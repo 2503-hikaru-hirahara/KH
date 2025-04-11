01a36217b46c4cdd834b18f965d860fb64179373
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-11 13:58:44 +0900

    ホーム画面①（form,repository,KhApplication,build.gradle)

--- src/main/java/com/example/KH/controller/form/TaskForm.java
+++ src/main/java/com/example/KH/controller/form/TaskForm.java
@@ -1,4 +1,18 @@
 package com.example.KH.controller.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class TaskForm {
+
+    private int id;
+    private String content;
+    private short status;
+    private LocalDateTime limit_date;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
