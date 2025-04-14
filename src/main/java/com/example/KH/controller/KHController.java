24343c202f7a381d60acbcde15afceec25e6188b
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-14 14:08:23 +0900

    LocalDateTime型およびDateTime型をDate型に統一

--- src/main/java/com/example/KH/controller/KHController.java
+++ src/main/java/com/example/KH/controller/KHController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -22,7 +22,7 @@ public class KHController {
     public ModelAndView top() {
         ModelAndView mav = new ModelAndView();
         // 現在日時を取得
-        LocalDate now = LocalDate.now();
+        Date now = new Date();
         // タスクを全件取得
         List<TaskForm> taskData = taskService.findAllTask();
         // 画面遷移先を指定
