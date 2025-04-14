0bd94f41f0edf1a4222d6d01f51a1f30abd62207
hirahara_hikaru <hirahara.hikaru@alhinc.jp>
2025-04-14 09:29:50 +0900

    ホーム画面③

--- src/main/java/com/example/KH/controller/KHController.java
+++ src/main/java/com/example/KH/controller/KHController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -20,10 +21,14 @@ public class KHController {
     @GetMapping
     public ModelAndView top() {
         ModelAndView mav = new ModelAndView();
+        // 現在日時を取得
+        LocalDateTime now = LocalDateTime.now();
         // タスクを全件取得
         List<TaskForm> taskData = taskService.findAllTask();
         // 画面遷移先を指定
         mav.setViewName("/top");
+        // 現在日時データオブジェクトを保管
+        mav.addObject("now", now);
         // タスクデータオブジェクトを保管
         mav.addObject("tasks", taskData);
         return mav;
